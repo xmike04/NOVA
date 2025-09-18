01d67e7997288490e245f7461c5a0c636fe71ece
Michael Marin <michaeldiaz32@my.unt.edu>
2025-09-17 20:32:34 -0500

    Security: Extract hardcoded NewsAPI key to config
    - Moved hardcoded NewsAPI key from news.py to config.py
    - Updated news.py to import and use config.news_api_key
    - Maintained functionality while improving security
    - All API keys now centralized in config files
    - Template config updated with NewsAPI key placeholder

--- NOVA/config/config_template.py
+++ NOVA/config/config_template.py
@@ -6,5 +6,5 @@ email_password = "<your_email_password>"
 weather_api_key = "<your_openweathermap_api_key>"
 wolframalpha_id = "<your_wolframalpha_id>"
 
-# News API (currently using a hardcoded key, but you can get your own)
+# News API
 news_api_key = "ae5ccbe2006a4debbe6424d7e4b569ec"  # Replace with your own NewsAPI key
--- NOVA/features/news.py
+++ NOVA/features/news.py
@@ -1,19 +1,18 @@
 import requests
 import json
-
+from NOVA.config import config
 
 
 def get_news():
-    url = 'http://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=ae5ccbe2006a4debbe6424d7e4b569ec'
+    url = f'http://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey={config.news_api_key}'
     news = requests.get(url).text
     news_dict = json.loads(news)
     articles = news_dict['articles']
     try:
-
         return articles
     except:
         return False
 
 
 def getNewsUrl():
-    return 'http://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=ae5ccbe2006a4debbe6424d7e4b569ec'
+    return f'http://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey={config.news_api_key}'
